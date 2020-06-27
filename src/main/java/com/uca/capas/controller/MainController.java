884be0a6af6634a6710ef0da362364ba975683af
Bryanceren <00139817@uca.edu.sv>
2020-06-27 05:05:34 -0600

    esqueleto

---
+++ src/main/java/com/uca/capas/controller/MainController.java
@@ -0,0 +1,30 @@
+package com.uca.capas.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	
+	
+	@RequestMapping("/login")
+	public ModelAndView login() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("login");
+		return mav;
+	}
+	
+	@RequestMapping("/register")
+	public ModelAndView register() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("register");
+		return mav;
+	}
+	
+	
+	
+	
+	
+}
