b29c443e8b1e35e6cd176c3d7a6c184366726bc8
f-molina <00300917@uca.edu.sv>
2020-06-30 20:10:51 -0600

    estudiante repository, metodo buscar por id

---
+++ src/main/java/com/uca/capas/repositories/EstudianteRepository.java
@@ -0,0 +1,14 @@
+package com.uca.capas.repositories;
+
+import com.uca.capas.domain.Estudiante;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface EstudianteRepository extends JpaRepository<Estudiante, Integer> {
+
+    @Query(value = "SELECT * from public.estudiante where id = :estudiante", nativeQuery = true)
+    public Estudiante findEstudianteById(@Param("estudiante") Integer estudiante);
+    
+}
