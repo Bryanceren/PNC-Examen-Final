165a387c654b9816c0d2150f90a84001ede87a5b
f-molina <00300917@uca.edu.sv>
2020-07-03 19:49:08 -0600

    buscar estudiante y plantilla para guardar

--- src/main/java/com/uca/capas/controller/CoordinadorController.java
+++ src/main/java/com/uca/capas/controller/CoordinadorController.java
@@ -2,15 +2,20 @@ package com.uca.capas.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.uca.capas.domain.Estudiante;
+import com.uca.capas.domain.EstudianteMateria;
+import com.uca.capas.domain.Materia;
 import com.uca.capas.dto.EstudianteDTO;
 import com.uca.capas.service.EstudianteService;
+import com.uca.capas.service.MateriaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -19,39 +24,54 @@ public class CoordinadorController {
     @Autowired
     EstudianteService estudianteService;
 
+    @Autowired
+    MateriaService materiaService;
+
     @RequestMapping("/materias-cursadas")
-    public ModelAndView lolazo() {
+    public ModelAndView indexMaterias() {
         ModelAndView mav = new ModelAndView();
-        mav.addObject("estudiante", new Estudiante());
         mav.setViewName("materias-cursadas");
 
         return mav;
     }
 
-    @RequestMapping("/buscarEstudiante")
-	public ModelAndView buscarEst(@RequestParam Integer codigo) {
-		ModelAndView mav = new ModelAndView();
-		Estudiante e = estudianteService.findOne(codigo);
-		mav.addObject("estudiante", e);
-		mav.setViewName("materias-cursadas");
-		return mav;
-    }
-
     @RequestMapping("/buscarEstudianteModal")
 	public ModelAndView buscarEstModal(@RequestParam Integer codigo) {
 		ModelAndView mav = new ModelAndView();
-		Estudiante e = estudianteService.findOne(codigo);
-		mav.addObject("estudiante", e);
+        Estudiante e = estudianteService.findOne(codigo);
+        List<Materia> materias = null;
+        try {
+            materias = materiaService.findAllMaterias();
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+        mav.addObject("estumat", new EstudianteMateria());
+        mav.addObject("materias", materias);
+		mav.addObject("estudiantes", e);
 		mav.setViewName("agregar-materia-cursada");
 		return mav;
     }
-    
-    //test criteria buscar por nombre de materia
-    @RequestMapping("/getEstudiantesMaterias")
-    public @ResponseBody List<EstudianteDTO> getEstuMateria(@RequestParam String nombre) {
-        List<EstudianteDTO> estudiantes = estudianteService.getEstudianteMateria(nombre);
 
-        return estudiantes;
+    //buscar estudiante materias-cursadas
+    @RequestMapping("/buscarEstudiante")
+    public ModelAndView buscarEstu(@RequestParam Integer codigo) {
+        ModelAndView mav = new ModelAndView();
+        List<EstudianteDTO> estudiantes = estudianteService.getEstudianteMateriaCodigo(codigo);
+        mav.addObject("estudiantes", estudiantes);
+        mav.setViewName("materias-cursadas");
+
+        return mav;
+    }
+
+    @RequestMapping("/guardarNota")
+    public ModelAndView guardarNota(@Valid @ModelAttribute EstudianteMateria estumat, @ModelAttribute Estudiante estudiantes) {
+        ModelAndView mav = new ModelAndView();  
+        estumat.setEstudiante(estudiantes);
+        materiaService.save(estumat);
+        mav.setViewName("materias-cursadas");
+
+        return mav;
+
     }
     
 }
